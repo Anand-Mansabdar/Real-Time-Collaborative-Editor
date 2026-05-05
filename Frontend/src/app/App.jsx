c763fcfb86b2383d5c51acd104bcc9e8f8752856
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-05-05 18:43:58 +0530

    task: Setting Up YJS in React(frontend)

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
-import "./App.css"
-import {Editor} from "@monaco-editor/react"
+import React, { useMemo, useRef } from "react";
+import "./App.css";
+import { Editor } from "@monaco-editor/react";
+import { MonacoBinding } from "y-monaco";
+import * as Y from "yjs";
+import { SocketIOProvider } from "y-socket.io";
 
 const App = () => {
-  return (
-    <main className='h-screen w-full bg-neutral-950 text-amber-50 flex gap-4 p-4'>
-      <aside className='h-full w-1/4 bg-rose-400 rounded-lg'>
-      </aside>
+  const editorRef = useRef(null);
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const ytext = useMemo(() => ydoc.getText("monaco"), [ydoc]);
 
-      <section className='w-3/4 h-full bg-rose-500 rounded-lg overflow-hidden'>
-        <Editor height={"100%"} defaultLanguage='javascript' defaultValue='// Start writing here' theme='vs-dark' />
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+
+    const provider = new SocketIOProvider(
+      "http://localhost:3000",
+      "monaco",
+      ydoc,
+      {
+        autoConnect: true
+      }
+    );
+
+    const monacoBinding = new MonacoBinding(
+      ytext,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness,
+    );
+  };
+
+  return (
+    <main className="h-screen w-full bg-neutral-950 text-amber-50 flex gap-4 p-4">
+      <aside className="h-full w-1/4 bg-rose-400 rounded-lg"></aside>
+
+      <section className="w-3/4 h-full bg-rose-500 rounded-lg overflow-hidden">
+        <Editor
+          height={"100%"}
+          defaultLanguage="javascript"
+          defaultValue="// Start writing here"
+          theme="vs-dark"
+          onMount={handleMount}
+        />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
